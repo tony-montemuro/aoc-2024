aa4fbc714c7339c8556a26c65da28d45a3da21f4
tony-montemuro <tonyamontemuro@gmail.com>
2024-12-02 19:36:39 -0600

    added more reading methods

--- aocutils/read.go
+++ aocutils/read.go
@@ -3,6 +3,7 @@ package aocutils
 import (
 	"log"
 	"os"
+	"strings"
 )
 
 func GetRawInput() string {
@@ -13,4 +14,30 @@ func GetRawInput() string {
 	}
 
 	return string(data)
+}
+
+func GetInputRows() []string {
+	input := GetRawInput()
+	return strings.Split(input, "\n")
+}
+
+func GetIntMatrixInput(separator string) [][]int {
+	input := GetInputRows()
+	matrix := make([][]int, len(input))
+
+	for y, str := range input {
+		nums := strings.Split(str, separator)
+		matrix[y] = make([]int, len(nums))
+
+		for x, n := range nums {
+			matrix[y][x] = Stoi(n)
+		}
+	}
+
+	return matrix
+}
+
+func GetSingleRowInputs(separator string) []string {
+	input := GetRawInput()
+	return strings.Split(input, separator)
 }
